a589b770dc5419a33e46e7a0b2ebf0605e07d24a
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2023-08-26 10:19:53 +0530

    Area and circumference of a circle

--- 1.java
+++ 1.java
@@ -1,1 +1,12 @@
 //Write a JavaScript program to perform find the area and circumference of a circle
+public class Arithmatics {
+    public static void main(String args[]) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println("Sum = "+(num1+num2));
+        System.out.println("Difference = "+(num1-num2));
+        System.out.println("Product = "+(num1*num2));
+        System.out.println("Quotient = "+(num1/num2));
+        System.out.println("Reminder = "+(num1%num2));
+    }
+}
