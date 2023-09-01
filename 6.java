06d2cda8f5295536a303a9ab2ed327e49000a115
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2023-09-01 18:20:22 +0530

    HCF LCM of Two Numbers

---
+++ 6.java
@@ -0,0 +1,28 @@
+//6- HCF LCM of Two Numbers
+import java.util.Scanner;
+public class HcfLcm {
+    public static void main(String[] args) {
+        int a,b,temp, t1,t2,hcf, lcm;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two numbers : ");
+            a = sc.nextInt();
+            b = sc.nextInt();
+        }
+        t1=a;
+        t2=b;
+        while(t2!=0){
+            temp=t2;
+            t2=t1%t2;
+            t1=temp;
+        }
+        hcf=t1;
+        System.out.println("HCF of entered number is : "+hcf);
+        lcm=(a*b)/hcf;
+        System.out.println("LCM of entered number is : "+lcm);
+    }
+}
+
+/*Enter First Number: 10
+Enter Second Number: 4
+HCF of input numbers: 2
+LCM of input numbers: 20*/
