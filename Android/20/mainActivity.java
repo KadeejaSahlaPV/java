9614bc2c96317eb8736153d9c686b6a7b7535da2
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2024-01-18 22:19:09 +0530

    Activity using intent

---
+++ Android/20/mainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void gotosecond(View v){
+    	Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+    	startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
